193207ce024ba429fc9640e68412333d7dae3dc6
Artem <artemkolesnik0201@gmail.com>
2025-06-15 15:44:19 +0100

    1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,87 +1,70 @@
-import {
-  keepPreviousData,
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-import { createNote, deleteNote, fetchNotes } from "../../services/noteService";
-import NoteList from "../NoteList/NoteList";
-import type { createNoteValues } from "../../types/note";
-
 import css from "./App.module.css";
 import { useState } from "react";
-import Pagination from "../Pagination/Pagination";
-import NoteModal from "../NoteModal/NoteModal";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
+import NoteList from "../NoteList/NoteList";
 import SearchBox from "../SearchBox/SearchBox";
+import Pagination from "../Pagination/Pagination";
 import { useDebounce } from "use-debounce";
-import Loader from "../Loader/Loader";
+import NoteModal from "../NoteModal/NoteModal";
+import SortFilter from "../SortFilter/SortFilter";
+import type { Tag } from "../../types/note";
 
-function App() {
-  const queryClient = useQueryClient();
+export default function App() {
+  const [searchQuery, setQuery] = useState("");
+
+  const [sortQuery, setSortQuery] = useState<Tag>("Personal");
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [searchQuery, setSearchQuery] = useState("");
-  const [debouncedQuery] = useDebounce(searchQuery, 300);
+  const [modalOnClose, setModalOnClose] = useState(false);
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["todos", currentPage, debouncedQuery],
-    queryFn: () => fetchNotes(currentPage, 10, debouncedQuery),
-    placeholderData: keepPreviousData,
+  const onPageChange = ({ selected }: { selected: number }) =>
+    setCurrentPage(selected + 1);
+
+  const [debounceQuery] = useDebounce(searchQuery, 500);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["tasks", debounceQuery, currentPage, sortQuery],
+    queryFn: () => fetchNotes({ searchQuery, currentPage, sortQuery }),
+    // enabled: query !== "",
   });
 
   const totalPages = data?.totalPages ?? 0;
 
-  const createMutation = useMutation({
-    mutationFn: createNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["todos"] });
-    },
-  });
-
-  const handleCreateNote = (newTodo: createNoteValues) => {
-    createMutation.mutate(newTodo);
+  const createNoteBtn = () => {
+    setModalOnClose(true);
   };
 
-  const deleteMutation = useMutation({
-    mutationFn: deleteNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["todos"] });
-    },
-  });
-
-  const handleDeleteNote = (id: number) => {
-    deleteMutation.mutate(id);
+  const closeModal = () => {
+    setModalOnClose(false);
   };
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox value={searchQuery} onSearch={setSearchQuery} />
-        {data && totalPages > 1 && (
+        <SearchBox value={searchQuery} onSearch={setQuery} />
+
+        <SortFilter changeTag={setSortQuery} />
+
+        {isSuccess && totalPages > 1 && (
           <Pagination
             totalPages={totalPages}
             currentPage={currentPage}
-            onPageChange={setCurrentPage}
+            onPageChange={onPageChange}
           />
         )}
-        {
-          <button className={css.button} onClick={() => setIsModalOpen(true)}>
-            Create note +
-          </button>
-        }
+
+        <button onClick={createNoteBtn} className={css.button}>
+          Create note +
+        </button>
       </header>
-      {isLoading && <Loader />}
-      {data && <NoteList notes={data.notes} onDelete={handleDeleteNote} />}
-      {isModalOpen && (
-        <NoteModal
-          onClose={() => setIsModalOpen(false)}
-          onSubmitNote={handleCreateNote}
-        />
+
+      {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
+
+      {modalOnClose && (
+        <NoteModal onClose={closeModal} onSuccess={closeModal} />
       )}
     </div>
   );
 }
-
-export default App;
